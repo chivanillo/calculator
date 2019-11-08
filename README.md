06d13577136999aa09cfb41b070f84b7109d5660
Ruiz Ruiz,Jose <Jose.RuizRuiz@Cerner.com>
2019-11-08 15:17:46 +0000

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# calculator
- The Calculator App by Jose Ruiz Ruiz
---
+++ precache-manifest.af21129cf8c8a870eae09596144f4c0a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0240902bc05944a930dac4d0d0e49e9",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "10db021f8a1400dcab1f",
+    "url": "/calculator/static/js/2.fd96d03f.chunk.js"
+  },
+  {
+    "revision": "a6827af5ca062bb88784",
+    "url": "/calculator/static/js/main.b028bd8a.chunk.js"
+  },
+  {
+    "revision": "81eb61f5a8d26d2fd52d",
+    "url": "/calculator/static/js/runtime-main.1d26180e.js"
+  }
+]);
